9217a4658a54ad7ff9902bbfbcd672c361b2dc56
KFlamsted <flamsted@gmail.com>
2026-02-07 22:51:33 +0100

    Adding responsive grid from material ui to handle socials responsiveness

--- react-thin-client/src/components/SocialsList/SocialsList.tsx
+++ react-thin-client/src/components/SocialsList/SocialsList.tsx
@@ -1,13 +1,28 @@
+import { Grid } from '@mui/material';
 import SocialCard from './SocialCard';
 import { socialMediaData } from './dummyData';
 import './SocialsList.css';
 
 const SocialsList = () => {
+  const shouldUseGrid = socialMediaData.length > 5;
+
   return (
     <div className="socials-list">
-      {socialMediaData.map((social, index) => (
-        <SocialCard key={social.platform} social={social} index={index} />
-      ))}
+      {shouldUseGrid ? (
+        <Grid container spacing={2}>
+          {socialMediaData.map((social, index) => (
+            <Grid key={social.platform} size={{ xs: 12, sm: 6 }}>
+              <SocialCard social={social} index={index} />
+            </Grid>
+          ))}
+        </Grid>
+      ) : (
+        <div className="socials-list-vertical">
+          {socialMediaData.map((social, index) => (
+            <SocialCard key={social.platform} social={social} index={index} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
